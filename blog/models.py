701ec0316f62a7c6e7e92c916a90408d90f7d86d
Dan Claudiu Pop <danclaudiupop@gmail.com>
2012-11-04 18:08:04 +0200

    adding category model

--- blog/models.py
+++ blog/models.py
@@ -5,6 +5,14 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     body = models.TextField()
     created = models.DateTimeField()
+    category = models.ManyToManyField("Category")
+
+    def __unicode__(self):
+        return self.title
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=100)
 
     def __unicode__(self):
         return self.title
