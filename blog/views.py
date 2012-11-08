36b6cf667a4571860ce62fabc8d3fc550bee8bac
Dan Claudiu Pop <danclaudiupop@gmail.com>
2012-11-08 10:05:58 +0200

    filter posts based on publish status

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from blog.models import Post
 
+
 def index(request):
-    latest_posts = Post.objects.order_by('-created')[:5]
+    latest_posts = Post.objects.filter(published=True).order_by('-created')[:5]
     context = {'latest_posts': latest_posts}
     return render(request, 'posts.html', context)
