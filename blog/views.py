4e7a57c0bde608c76e03b2626b7d05b2ba9ae50e
Dan Claudiu Pop <danclaudiupop@gmail.com>
2012-11-17 19:18:34 +0200

    template gardening

--- blog/views.py
+++ blog/views.py
@@ -5,4 +5,4 @@ from blog.models import Post
 def index(request):
     latest_posts = Post.objects.filter(published=True).order_by('-created')[:5]
     context = {'latest_posts': latest_posts}
-    return render(request, 'posts.html', context)
+    return render(request, 'blog.html', context)
