5eef3ad4a159f7a2e2aedb6ecfbd629663f4c647
Dan Claudiu Pop <danclaudiupop@gmail.com>
2012-11-19 20:28:11 +0200

    improved urls

--- blog/urls.py
+++ blog/urls.py
@@ -2,9 +2,9 @@ from django.conf.urls import patterns, url
 
 
 urlpatterns = patterns('blog.views',
-        url(r'^$', 'index'),
-        url(r'^archive$', 'archive'),
-        url(r'^tag/(?P<tag>\w+)$', 'tag'),
-        url(r'^category/(?P<cat>\w+)$', 'category'),
-        url(r'^(?P<slug>[^\.]+)', 'post'),
+    url(r'^$', 'index'),
+    url(r'^archive/$', 'archive'),
+    url(r'^tag/(?P<tag>\w+)/$', 'tag'),
+    url(r'^category/(?P<cat>\w+)/$', 'category'),
+    url(r'^(?P<slug>[^\.]+)/$', 'post'),
 )
