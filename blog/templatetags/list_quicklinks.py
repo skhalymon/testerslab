9e42c6a1cf9e66e26be34981b351318bfff71ebf
Dan Claudiu Pop <danclaudiupop@gmail.com>
2012-12-19 13:41:15 +0200

    retrieve quick links randomly

--- blog/templatetags/list_quicklinks.py
+++ blog/templatetags/list_quicklinks.py
@@ -7,7 +7,7 @@ from blog.models import QuickLink
 
 
 def get_quicklinks():
-    quick_links = QuickLink.objects.filter(visible=True)
+    quick_links = QuickLink.objects.filter(visible=True).order_by('?')
     return {'quick_links': quick_links}
 
 register.inclusion_tag('list_quicklinks.html')(get_quicklinks)
